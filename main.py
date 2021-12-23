3db37c3b8cf9f77bfe6000b1fa38716b96f17053
Evair Macollunco <evair-dev@Evairs-MacBook-Pro.local>
2021-12-23 07:02:20 -0500

    Learn about path parameters

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
-app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
-@app.get("/")
+app = FastAPI()  # change app => text
+
+
+@app.get("/")   # change app => @texts
 async def root():
     return {"message": "Hello World"}
+
+
+# fixed path should be declared at the beginning
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    print(model_name, model_name.value)  # object, value
+    print(ModelName.alexnet, ModelName.alexnet.value)  # object, value
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
